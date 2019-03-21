a510b9ae28da8ae874a78f52f3b05df741d82967
Bruno Amorim <amorim.bac@gmail.com>
2019-03-21 18:09:22 +0000

    progress

--- src/evalblochham.jl
+++ src/evalblochham.jl
@@ -5,3 +5,13 @@ function hamiltoniank!(mat::AbstractMatrix, sys::BlochHamiltonian, k::AbstractVe
     sys.hk!(mat, SVector{D}(k))
 
 end
+
+function hamiltoniank(sys::BlochHamiltonian, k::AbstractVector)
+    @assert length(k) == D "length of k vector much be the same as the dimensionality of the system"
+
+    dim = norbs()
+    mat = zeros(Complex{Float64}, )
+
+    sys.hk!(mat, SVector{D}(k))
+
+end
